106c5c275734de2375ee9cabcc928bdd30be8a17
William Fugate <136034758+wfugate@users.noreply.github.com>
2024-10-07 20:52:29 -0400

    Add files via upload

---
+++ wildlife_tracker/animal_management/animal.py
@@ -0,0 +1,20 @@
+from typing import Any, Optional
+
+class Animal:
+
+    def __init__(self, animal_id: int, age: Optional[int] = None, health_status: Optional[str] = None):
+        self.animal_id = animal_id
+        self.age = age
+        self.health_status = health_status
+
+            
+    def get_animal_details(animal_id) -> dict[str, Any]:
+        pass
+
+    def update_animal_details(animal_id: int, **kwargs: Any) -> None:
+        pass
+
+    
+    
+
+
